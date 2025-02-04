fead788d476fd5224370910349ac7e4e2b7f3b07
Mahmoud Yasser <52430441+Yasser-Cloud@users.noreply.github.com>
2025-02-04 08:23:51 +0200

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,38 @@
+
+
+
+from fastai.vision.all import *
+
+# Define the path to your dataset
+path = Path("herb_dataset/dataset_split")
+
+# Create a DataBlock
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),  
+    get_items=get_image_files,          
+    splitter=GrandparentSplitter(train_name='train', valid_name='test'),  
+    get_y=parent_label,               
+    item_tfms=Resize(224, method='squish'),              # Resize images to 224x224
+    batch_tfms=aug_transforms(max_rotate=30, max_zoom=1.5, max_lighting=0.2)         # Apply data augmentation
+).dataloaders(path, bs=16)              # Create DataLoaders with batch size 32
+
+
+
+learn = vision_learner(
+    dls,
+    convnext_base,
+    metrics=accuracy
+)
+
+# Add EarlyStoppingCallback and SaveModelCallback
+cbs = [
+    EarlyStoppingCallback(monitor='valid_loss', patience=5),  # Stop training if validation loss doesn't improve for 5 epochs
+    SaveModelCallback(monitor='valid_loss', fname='best_model')  # Save the best model based on validation loss
+]
+# Train the model
+learn.fine_tune(epochs=20,cbs=cbs,base_lr=  0.001737800776027143)
+
+
+# Wow not bad but unfortunately, the model is too big. I want a small one for production it's 338.2 MB
+
+#will find your model in models folder
